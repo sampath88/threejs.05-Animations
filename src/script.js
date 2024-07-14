c0fb5b129f62cb2d4d38d763ed9fc551527dec1f
sampath88 <sampathreddy8896@gmail.com>
2024-07-14 18:32:29 +0530

    using gsap for animation

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import gsap from "gsap";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -40,14 +41,10 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 
-let clock = new THREE.Clock();
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
 // Animations
 const tick = () => {
-  let elapsedTime = clock.getElapsedTime();
-  camera.position.y = Math.sin(elapsedTime);
-  camera.position.x = Math.cos(elapsedTime);
-  camera.lookAt(mesh.position);
-
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
